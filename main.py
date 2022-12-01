2b426c73753f75f954995224037851066671fffd
GoncharukAK <GoncharukAK@gaz.ru>
2022-12-01 10:53:36 +0300

    Презентационная версия

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from hashlib import md5
+
 from web.web_server import start_server
 
 if __name__ == '__main__':
-    start_server()
+    passwd = input('Пароль базы: ').encode('utf-8')
+    print(md5('zagfa123'.encode('utf-8')).hexdigest())
+    print(type(md5(passwd).hexdigest()))
+    if md5(passwd).hexdigest() != 'b703ad2d5ae6b286fb52bd5cb955d02b':
+        print('не верный пароль!')
+    else:
+        start_server()
--- web/web_server.py
+++ web/web_server.py
@@ -24,7 +24,7 @@ def unit():
 
 
 def start_server(debug=False):
-    app.run(debug=debug)
+    app.run(debug=debug, host="0.0.0.0")
 
 
 if __name__ == '__main__':
